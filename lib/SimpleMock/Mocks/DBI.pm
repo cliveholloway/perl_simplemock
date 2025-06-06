2d3d06ed7c7ae47fd903fb6e8456cc02011c8253
Clive Holloway <clive.holloway@gmail.com>
2025-06-05 22:29:29 -0400

    Add missing files

---
+++ lib/SimpleMock/Mocks/DBI.pm
@@ -0,0 +1,15 @@
+package SimpleMock::Mocks::DBI;
+use strict;
+use warnings;
+
+no warnings 'redefine';
+            
+my $orig_connect = \&DBI::connect;
+     
+# force DBI connect to use dbd:SimpleMock
+*DBI::connect = sub {
+    my ($class, undef, undef, undef, $attr) = @_;
+    return $orig_connect->($class, 'dbi:SimpleMock:', undef, undef, $attr);
+};
+
+1;
